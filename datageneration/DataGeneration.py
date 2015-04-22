908593c7c02f65c051b9dbf30a65c62a9d7ade1b
Andrea Suckro <andrea_suckro@intuit.com>
2015-04-22 16:36:25 +0200

    Enhanced Datageneration Script and lay down basic project structure for the extraction. Made changes to the gitignore for IntelliJ files.

--- datageneration/DataGeneration.py
+++ datageneration/DataGeneration.py
@@ -1,65 +1,84 @@
 # -*- coding: utf-8 -*-
 """
-Creates dump test data for the ab test service.
+Creates dump test data for the ab test service with different distributions.
 """
 import numpy as np
 import matplotlib.pyplot as plt
 import urllib2
 import json
 
-sigma = 0.1
-muNorm = 0
-muDiff = 0.4
-
-numUsers = 600
-
-expName = 'Bachelor4'
-
-sNormal = np.random.normal(muNorm,sigma,numUsers)
-count, bins, ignored = plt.hist(sNormal, 30, normed=True)
-plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - muNorm)**2 / (2 * sigma**2) ),linewidth=2, color='r')
-plt.show()
 
 
-sDiff = np.random.normal(muDiff,sigma,numUsers)
-count, bins, ignored = plt.hist(sDiff, 30, normed=True)
-plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - muDiff)**2 / (2 * sigma**2) ),linewidth=2, color='g')
-plt.show()
+expName = 'Bachelor11'
+bucketControll = 'a'
+bucketTest = 'b'
+
+#Jabba API calls
+server = "http://localhost:8080/api/"
+events_api = "v1/events/applications/"
+assignments_api = "v1/assignments/applications/"
+
+ct = {'Content-Type':'application/json'}
 
 
-normalDic = {}
-diffDic = {}
+#################################
+# Helper Functions for API-Calls
+#################################
 
-#for value in s set out http request
-userId = 0
-for val in sNormal:
-    ans = json.loads(urllib2.urlopen("http://localhost:8080/api/v1/assignments/applications/qbo/experiments/"+expName+"/users/"+str(userId)).read())  
-    userId+=1
-    #store the information value, assignment, userId
-    normalDic[val] = [ans["assignment"],userId]
-    #record impression
-    body = "{\"events\":[{\"name\":\"IMPRESSION\"}]}"
-    req = urllib2.Request("http://localhost:8080/api/v1/events/applications/qbo/experiments/"+expName+"/users/"+str(userId),body,{'Content-Type':'application/json'})
-    urllib2.urlopen(req)    
-    #post event!
-    if val > 0 :
-        body = "{\"events\":[{\"name\":\"Bought\"}]}"
-        urllib2.urlopen(req)
-      
-    
-        
-    
-for val in sDiff:
-    ans = json.loads(urllib2.urlopen("http://localhost:8080/api/v1/assignments/applications/qbo/experiments/"+expName+"/users/"+str(userId)).read())
-    userId+=1
-    diffDic[val] = [ans["assignment"],userId]
-    body = "{\"events\":[{\"name\":\"IMPRESSION\"}]}"
-    req = urllib2.Request("http://localhost:8080/api/v1/events/applications/qbo/experiments/"+expName+"/users/"+str(userId),body,{'Content-Type':'application/json'})
+def recordEvent(userId,event):
+    body = "{\"events\":[{\"name\":\""+event+"\"}]}"
+    req = urllib2.Request(server+events_api+"qbo/experiments/"+expName+"/users/"+str(userId),body,ct)
     urllib2.urlopen(req)
-    if val > 0 :
-        body = "{\"events\":[{\"name\":\"Bought\"}]}"
-        req = urllib2.Request("http://localhost:8080/api/v1/events/applications/qbo/experiments/"+expName+"/users/"+str(userId),body,{'Content-Type':'application/json'})
-        urllib2.urlopen(req)
-      
+
+#Impression is just an Event with a fixed value
+def recordImpression(userId):
+    recordEvent(userId,"IMPRESSION")
+
+def getAssignment(experiment, userId):
+    ans = json.loads(urllib2.urlopen(server+assignments_api+"qbo/experiments/"+experiment+"/users/"+str(userId)).read())  
+    return ans['assignment']
+    
+def createExp():
+    #TODO implement...
+    return 0
+
+
+
+##################################
+# Gauss Distributions 
+##################################
+numUsers = 500
+
+def getNormalDist(mu, sigma=0.1, display = 1):
+    data = np.random.normal(mu,sigma,numUsers)
+    if display:
+        count, bins, ignored = plt.hist(data, 30, normed=True)
+        plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ),linewidth=2, color='r')
+        plt.show()
+    return data
+
+
+
+###################################
+# MAIN PROGRAMM
+###################################
+
+#get 2 Gauss Distributions varing in mu - they represent the user
+sNormal = getNormalDist(0)
+sDiff = getNormalDist(0.1)
+
+
+for x in range(0,numUsers):
+    userId = x+1
+    ans = getAssignment(expName,userId)
+    recordImpression(userId)
+    if ans == 'a':
+        if sNormal[x] > 0:
+            recordEvent(userId,'check')
+    else:
+        if sDiff[x] > 0:
+            recordEvent(userId,'check')
+
+
 
 
